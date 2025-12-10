55706dc27b6e4e46e5c0543cd26401eca9f34e76
meenamanohar01 <meena.manohar01@gmail.com>
2025-12-11 01:36:09 +0530

    added CI CD Pipeline github actions yml file auto-pipeline.yml

--- tests/loginpage.spec.ts
+++ tests/loginpage.spec.ts
@@ -14,8 +14,23 @@ import {test, expect} from "../fixtures/basefixtures"
     expect(await homepage.isUserLoggedIn()).toBeTruthy();
   });   */
 
-test('verify valid login', async ({ homePage }) => {
+/* test('verify valid login', async ({ homePage }) => {
     await expect(homePage.page).toHaveTitle('My Account');
+}); */
+
+test('verify valid login @login',
+    {
+        annotation: [
+            { type: 'epic', description: 'EPIC 100 - Design login page for Open Cart App' },
+            { type: 'feature', description: 'Login Page Feature' },
+            { type: 'story', description: 'US 50 - user can login to app' },
+            { type: 'severity', description: 'Blocker' },
+            { type: 'owner', description: 'Naveen Khunteta'}
+        ]
+    }
+    , async ({ homePage }) => {
+        await expect(homePage.page).toHaveTitle('My Account');
+        
 });
 //In Invalid login we do not need Custome Fixture 
 //We need to destructure baseURL coming from playwright.config.ts to be given to goToLoginPage()
@@ -23,7 +38,8 @@ test('verify valid login', async ({ homePage }) => {
 test('verify invalid login', async ({ page, baseURL }) => {
     const loginPage = new LoginPage(page); //Isolated Page instance, created for each test. Pages are isolated between tests due to fixtures.context isolation.
     await loginPage.goToLoginPage(baseURL);
-    await loginPage.doLogin('meena.manohar01@gmail.com', 'test123');
+   // await loginPage.doLogin('meena.manohar01@gmail.com', 'test123');
+   await loginPage.doLogin('meena.manohar01@gmail.com', 'test123');
     const actualerrorMsg = await loginPage.getInvalidLoginMsg();
     console.log(actualerrorMsg);
     expect(actualerrorMsg).toContain('Warning: No match for E-Mail Address and/or Password.');
